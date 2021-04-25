23ac6e4575bf32896beb4b0271f0e6ac9d7e6fff
Razeev-stha <yuvrajstha1246@outlook.com>
2021-04-25 13:30:23 +0545

    useSWR hook to fetch data

--- pages/dummy-page.js
+++ pages/dummy-page.js
@@ -1,33 +1,42 @@
 import { useEffect, useState } from 'react'
+import useSWR from 'swr'
 
 const DummyPage = () => {
 	const [sales, setSales] = useState()
-	const [loading, setLoading] = useState(false)
+	// const [loading, setLoading] = useState(false)
+
+	const { data, error } = useSWR(
+		'https://nextjs-datafetching-5c813-default-rtdb.firebaseio.com/sales.json'
+	)
 	useEffect(() => {
-		setLoading(true)
-		fetch(
-			'https://nextjs-datafetching-5c813-default-rtdb.firebaseio.com/sales.json'
-		)
-			.then((response) => response.json())
-			.then((data) => {
-				const transformedSales = []
-				for (const key in data) {
-					transformedSales.push({
-						id: key,
-						username: data[key].username,
-						quantity: data[key].quantity,
-					})
-				}
-				setSales(transformedSales)
-				setLoading(false)
-			})
-	}, [])
-	if (loading) {
+		if (data) {
+			const transformedSales = []
+			for (const key in data) {
+				transformedSales.push({
+					id: key,
+					username: data[key].username,
+					quantity: data[key].quantity,
+				})
+			}
+			setSales(transformedSales)
+		}
+	}, [data])
+	// useEffect(() => {
+	// 	setLoading(true)
+	// 	fetch(
+	// 		'https://nextjs-datafetching-5c813-default-rtdb.firebaseio.com/sales.json'
+	// 	)
+	// 		.then((response) => response.json())
+	// 		.then((data) => {
+	// 			setLoading(false)
+	// 		})
+	// }, [])
+	if (error) {
+		return <p>Failed To Load</p>
+	}
+	if (!data || !sales) {
 		return <p>Loading..</p>
-    }
-    if (!sales) {
-        return <p>No data Yet</p>
-    }
+	}
 	return (
 		<ul>
 			{sales.map((sale) => (
